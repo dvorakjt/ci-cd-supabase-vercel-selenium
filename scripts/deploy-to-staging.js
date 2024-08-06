240bc7e58f3f867dc4ab1a98ce31d3fca635726b
Joseph Dvorak <dvorakjt@gmail.com>
2024-08-06 18:01:00 -0400

    added script to deploy to staging

---
+++ scripts/deploy-to-staging.js
@@ -0,0 +1,27 @@
+const { exec } = require("child_process");
+
+await deployAndSetPreviewURL();
+
+async function deployAndSetPreviewURL() {
+  const previewURL = await deployAndGetPreviewURL();
+  console.log(previewURL);
+}
+
+function deployAndGetPreviewURL() {
+  return new Promise((resolve, reject) => {
+    exec(
+      `vercel deploy --prebuilt --token=${process.env.VERCEL_TOKEN}`,
+      (error, stdout, stderr) => {
+        if (error) reject(error);
+
+        const previewURL = getPreviewURLFromOutput(stdout);
+        resolve(previewURL);
+      }
+    );
+  });
+}
+
+function getPreviewURLFromOutput(output) {
+  const pattern = /Preview: (\S+)/;
+  return pattern.exect(output)[1];
+}
