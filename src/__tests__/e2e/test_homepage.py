7f09d9e53891b59c63e7eb6a3444842c47599540
Joseph Dvorak <dvorakjt@gmail.com>
2024-08-07 03:17:27 -0400

    passed headless option into webdriver.Chrome

--- src/__tests__/e2e/test_homepage.py
+++ src/__tests__/e2e/test_homepage.py
@@ -2,12 +2,16 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 preview_url = os.environ['PREVIEW_URL']
 access_token = os.environ['ACCESS_TOKEN']
 url_with_access_token = preview_url + '?x-vercel-protection-bypass=' + access_token
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.implicitly_wait(10)
 driver.get(url_with_access_token)
 h1 = driver.find_element(By.XPATH, '//h1[text()="Hello World"]')
